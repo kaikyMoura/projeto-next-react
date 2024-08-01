8a09a6cd4d58a86149b77b21b3ba955049b5706e
kaiky de Moura <kaikymoura972@gmail.com>
2024-08-01 12:26:34 -0300

    04- Alterando visual do dashboard e adicionando dockerfile e docker-compose

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import SideMenu from "../Menu/SideMenu"
-import NavBar from "../Navbar/Navbar"
-import styles from './Dashboard.module.css'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faChartPie, faX } from "@fortawesome/free-solid-svg-icons";
 import { faChartLine } from "@fortawesome/free-solid-svg-icons/faChartLine";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import SideMenu from "../Menu/SideMenu";
+import NavBar from "../Navbar/Navbar";
+import styles from './Dashboard.module.css';
 
 interface DashBoardProps {
     children: React.ReactNode;
@@ -20,26 +20,30 @@ const DashBoard: React.FC<DashBoardProps> = ({ children }) => {
     return (
         <>
             <div className={`${styles.dashBoard}`}>
-                <NavBar ativarSideMenu={ativarSideMenu} navegacao={[
-                    { nome: "Home", link: "/home" }, { nome: "Conteudo", link: "/conteudo" }
+                <NavBar toggleSideBar={toggleSidebar} navegacao={[
+                    { nome: "Home", link: "/home" }, { nome: "Home", link: "/home" }
                 ]} titulo={"Dashboard"} />
 
-                <div className="">
-                    <button className={`mt-5 ${styles.toggleButton} ${!ativarSideMenu ? styles.expanded : null}`} onClick={toggleSidebar}><FontAwesomeIcon icon={faBars} /></button>
-                </div>
+                {!ativarSideMenu ?
+                <button className={`flex ${styles.toggleButton}`} onClick={toggleSidebar}>
+                    <FontAwesomeIcon icon={faX} color="white"/>
+                </button>
+                :null }
 
-                <SideMenu items={[
-                    { nome: "Dashboard", link: "/dashboard", icone: faChartLine }, { nome: "Produtos", link: "/produtos", icone: faCartShopping },
-                    { nome: "Venda", link: "/produtos", icone: faChartLine }, { nome: "Produtos", link: "/produtos", icone: faChartLine }
-                ]} ativarSideMenu={ativarSideMenu} />
+                {!ativarSideMenu ?
+                    <SideMenu items={[
+                        { nome: "Dashboard", link: "/dashboard", icone: faChartPie }, { nome: "Produtos", link: "/produtos", icone: faCartShopping },
+                        { nome: "Venda", link: "/produtos", icone: faChartLine }, { nome: "Produtos", link: "/produtos", icone: faChartLine }
+                    ]} ativarSideMenu={ativarSideMenu} />
+                    : null}
             </div>
-            
+
             <div className={styles.container}>
 
-            <main>
-                {children}
-            </main>
-            
+                <main>
+                    {children}
+                </main>
+
             </div>
         </>
     )
